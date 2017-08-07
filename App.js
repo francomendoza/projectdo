f0e94910defe77be25e6b9c5a6a98258658f1b44
francomendoza <franco@lingolive.com>
2017-08-06 22:49:19 -0400

    add native base and start basic UI

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Body, Title, Content, Button, Text } from 'native-base';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNewTask = this.handleNewTask.bind(this);
+
+    this.state = {
+      text: false,
+      tasks: []
+    };
+  }
+
+  handleNewTask() {
+    this.setState((prevState, props) => {
+      return {
+        text: !prevState.text
+      };
+    });
+  }
+
   render() {
+    let comp;
+    if (this.state.text) {
+      comp = <Text>Shake your phone to open the developer menu.</Text>;
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title>ProjectDo</Title>
+          </Body>
+        </Header>
+        <Button
+          onPress={this.handleNewTask}
+        ><Text>New Task</Text></Button>
+        {comp}
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
