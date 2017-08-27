7f518ff4ecb7cfee21da69a4c52def0fee218f60
francomendoza <franco@lingolive.com>
2017-08-27 16:37:47 -0400

    add redux

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import MissionControlApp from './components/MissionControlApp';
+import configStore from './redux/configStore';
+import { Provider } from 'react-redux';
+
+const store = configStore();
 
 export default class App extends React.Component {
   render() {
     return (
-      <MissionControlApp/>
+      <Provider store={store}>
+        <MissionControlApp/>
+      </Provider>
     );
   }
 }
