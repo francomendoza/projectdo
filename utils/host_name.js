45cc7081f998f490654e13323779b5f538bf60b4
francomendoza <franco@lingolive.com>
2017-09-05 18:46:18 -0400

    update hostname for production server

--- utils/host_name.js
+++ utils/host_name.js
@@ -1,2 +1,2 @@
 export const HOST_NAME = process.env.NODE_ENV === 'development' ?
-  'http://localhost:3000' : 'https://192.168.1.3:3000'
+  'http://localhost:3000' : 'https://projectmissioncontrol.herokuapp.com'
