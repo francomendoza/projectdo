3396d63c5f0a6df038f2dc5903ef8c031fe08d3f
francomendoza <franco@lingolive.com>
2017-12-02 14:12:42 -0500

    add mission abort

--- components/TaskListItem/index.js
+++ components/TaskListItem/index.js
@@ -20,54 +20,62 @@ import { HOST_NAME } from '../../utils/host_name';
 export default class TaskListItem extends React.Component {
   constructor(props) {
     super(props);
-    this.onRightActionRelease = this.onRightActionRelease.bind(this);
+    this.updateMissionStatus = this.updateMissionStatus.bind(this);
   }
 
-  onRightActionRelease() {
-    fetch(`${HOST_NAME}/missionstatus/update`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        mission_id: this.props.item.id,
-        status: 'complete',
+  updateMissionStatus(status) {
+    return () => {
+      fetch(`${HOST_NAME}/missionstatus/update`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          mission_id: this.props.item.id,
+          status: status,
+        })
       })
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`Network response failed somehow.`);
-    })
-    .then(data => {
-      if (data.status == 'SUCCESS') {
-        // for now just remove mission from list
-        this.props.dispatch(removeMission(this.props.item.id));
-      }
-    })
-    .catch((error) => {
-      Alert.alert(
-        'Sorry',
-        `Fetch failed sorry dude: ${error}`
-      );
-    });
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(`Network response failed somehow.`);
+      })
+      .then(data => {
+        if (data.status == 'SUCCESS') {
+          // for now just remove mission from list
+          this.props.dispatch(removeMission(this.props.item.id));
+        }
+      })
+      .catch((error) => {
+        Alert.alert(
+          'Sorry',
+          `Fetch failed sorry dude: ${error}`
+        );
+      });
+    };
   }
+
 
   render() {
     const rightButtons = [
       <TouchableHighlight
         style={{flex: 1, justifyContent: 'center', backgroundColor: '#54a3ff'}}
-        onPress={this.onRightActionRelease}>
+        onPress={this.updateMissionStatus('complete')}>
         <Text style={{paddingLeft: 6, color: 'white'}}>Complete</Text>
+      </TouchableHighlight>,
+      <TouchableHighlight
+        style={{flex: 1, justifyContent: 'center', backgroundColor: '#54a3ff'}}
+        onPress={this.updateMissionStatus('aborted')}>
+        <Text style={{paddingLeft: 6, color: 'white'}}>Abort</Text>
       </TouchableHighlight>,
     ];
 
     return (
       <Swipeable
         rightButtons={rightButtons}
-        onRightActionRelease={this.onRightActionRelease}
+        onRightActionRelease={this.updateMissionStatus('complete')}
         rightActionActivationDistance={300}
         onSwipeStart={this.props.setSectionListScroll(false)}
         onSwipeRelease={this.props.setSectionListScroll(true)}
